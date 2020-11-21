76bbbc2dda7ed90531f5a8b2eec7d8bd37f68757
MadhumithaS2001 <52739023+MadhumithaS2001@users.noreply.github.com>
2020-11-21 21:17:26 +0530

    Update switch.c

--- switch.c
+++ switch.c
@@ -1,15 +1,27 @@
 #include<stdio.h>
-main()
+int main()
 {
-   int i;
-   scanf("%d",&i);
-   switch(i)
-   {
-      case 1:
-          
-      case 2:
-      case 3:
-      case 4:
-      case 5:
+    int i;
+    scanf("%d",&i);
+    switch(i)
+    {
+        case 1:
+          printf("Food Item - Pizza\nPrice - Rs.239");
+          break;
+        case 2:
+          printf("Food Item - Burger\nPrice - Rs.129");
+          break;
+        case 3:
+          printf("Food Item - Pasta\nPrice - Rs.179");
+          break;
+        case 4:
+          printf("Food Item - French Fries\nPrice - Rs.99");
+          break;
+        case 5:
+          printf("Food Item - Sandwitch\nPrice - Rs.149");
+          break;
+        default:
+            break;
    }
+   return 0;
 }
