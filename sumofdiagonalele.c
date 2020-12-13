ec32b71c69ce1d699d0ae05c7c89f7aa6d1500ab
MadhumithaS2001 <52739023+MadhumithaS2001@users.noreply.github.com>
2020-12-13 23:42:28 +0530

    Update sumofdiagonalele.c

--- sumofdiagonalele.c
+++ sumofdiagonalele.c
@@ -4,8 +4,19 @@ int main()
     int i,j,mat[10][10];
     printf("Enter the elements of 3x3 MATRIX\n");
     for(i=0;i<3;i++)
-        for(j=0;j<3;j++)
+    {
+        for(j=0;j<3;j++){
+            printf("element-[%d][%d] = ",i,j);
             scanf("%d",&mat[i][j]);
+        }
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++){
+            printf("%d\t",mat[i][j]);
+        }
+        printf("\n");
+    }
     int sum=0;
     for(i=0;i<3;i++)
     {
